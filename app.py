6325006003590e3bdae88134d3ccc0c2338064b5
kleix5 <98404414+kleix5@users.noreply.github.com>
2023-11-20 18:48:52 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -6,6 +6,35 @@ import json
 app = Flask(__name__)
 db = TinyDB('db.json')
 
+def get_collection(data):
+    collection = {}
+    for k, v in data.items():
+        ks = k.split('_')
+        if ks[0] == 'name':
+            collection[v] = data['type_' + ks[1]]
+        elif k == 'template_name':
+            collection['name'] = v
+    return collection
+
+def validation_replace(data):
+    val_array = []
+    for k, v in data.items():
+        if re.fullmatch(re_date, v):
+            val_array.append('date')
+        elif re.fullmatch(re_phone, v):
+            val_array.append('phone')
+        elif re.fullmatch(re_email, v):
+            val_array.append('email')
+        elif re.fullmatch(re_text, v):
+            val_array.append('text')
+        else:
+            val_array.append('Shit happens!')
+        result = len(val_array) == len(query)
+    if val_array[0] == 'Shit happens!':
+        return False, val_array
+    else:
+        return val_array
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -13,32 +42,16 @@ def index():
 @app.route('/add_form', methods=["POST"])
 def add_form():
     if request.method == "POST":
-        data = request.form.to_dict()
-        collection = {}
-        for k, v in data.items():
-            ks = k.split('_')
-            if ks[0] == 'name':
-                collection[v] = data['type_' + ks[1]]
-            elif k == 'template_name':
-                collection['name'] = v
-        db.insert(collection)
+        data = get_collection(request.form.to_dict())
+        db.insert(data)
         return "new tamplete was added"
     
 @app.route('/get_form', methods=["POST"])
 def get_form():
     if request.method == "POST":
-        data = request.form.to_dict()
-        collection = {}
-        for k, v in data.items():
-            ks = k.split('_')
-            if ks[0] == 'name':
-                collection[v] = data['type_' + ks[1]]
-            elif k == 'template_name':
-                collection['name'] = v
-        with open("get_form.json", "w") as my_file:
-            my_file.write(json.dumps(collection))
-            my_file.close()
-#         for k, v in collection.items():
+        data = get_collection(request.form.to_dict())
+        data = validation_replace(data)
+
             
 # всё, что нужно передаётся постом и пишется в файл, осталось реализовать валидацию и вернуть необходимые данные           
         return data
--- processtools.py
+++ processtools.py
@@ -11,27 +11,27 @@ re_date = re.compile(r'\d{1,4}\W\d{1,2}\W\d{1,4}')
 def valid(query):
     val_array = []
     for k, v in query.items():
-        if re.fullmatch(re_phone, v):
+        if re.fullmatch(re_date, v):
+            val_array.append('date')
+        elif re.fullmatch(re_phone, v):
             val_array.append('phone')
-        elif re.fullmatch(re_text, v):
-            val_array.append('text')
         elif re.fullmatch(re_email, v):
             val_array.append('email')
-        elif re.fullmatch(re_date, v):
-            val_array.append('date')
+        elif re.fullmatch(re_text, v):
+            val_array.append('text')
         else:
             val_array.append('Shit happens!')
         result = len(val_array) == len(query)
     if val_array[0] == 'Shit happens!':
-        return False
+        return False, val_array
     else:
         return  result, val_array
     
 
-def finder(in_dict, bd_array):
-    for i in bd_array:
-        if np.array_equal(np.sort(np.array(list(i))), np.sort(np.array(list(in_dict)))):
-            result = i['name']
+def finder(in_dict, db_array):
+    for item in db_array:
+        if np.array_equal(np.sort(np.array(list(item))), np.sort(np.array(list(in_dict)))):
+            result = item['name']
             break
         else:
             result = dict(zip(list(in_dict), valid(in_dict)[1]))
